c7acc74a2bc0daabff69c95f6768cfd4ab8d8c0b
Hisham <hisham.khashman@gmail.com>
2026-04-14 15:14:58 +0900

    fixing file for deployment2

--- data/projects.ts
+++ data/projects.ts
@@ -6,7 +6,7 @@ export type Project = {
   liveUrl: string;
   repoUrl?: string;
   category: "Current Projects" | "Selected Work";
-  subCategory?: "Full-Stack" | "Data" | "Security & Network";
+  subCategory?: "Full-Stack" | "Data" | "Security & Network" | "CLI Tools";
   badge?: string;
 };
 
@@ -28,6 +28,15 @@ export const projects: Project[] = [
     liveUrl: "https://hishamkhashman1.com/rick-and-morty-api",
     category: "Current Projects"
   },
+   {
+    title: "BugSquancher",
+    description: "Squanch the bug. Ship the fix.Coming soon.. your go to CLI debugger!",
+    stack: ["Python","Shell","PostgreSQL"],
+    image: "/projects/placeholder.svg",
+    liveUrl: "https://github.com/Hishamkhashman1/bug-squancher",
+    category: "Current Projects",
+    subCategory: "CLI Tools"
+  },		
   {
     title: "LinguaLogic",
     description: "Gamified coding learning platform.",
@@ -105,14 +114,5 @@ export const projects: Project[] = [
     liveUrl: "https://hisham-watch-list-aad8d178a962.herokuapp.com/",
     category: "Selected Work",
     subCategory: "Full-Stack"
-  },
-  {
-    title: "BugSquancher",
-    description: "Squanch the bug. Ship the fix.Coming soon.. your go to CLI debugger!",
-    stack: ["Python","Shell","PostgreSQL"],
-    image: "/projects/placeholder.svg",
-    liveUrl: "https://github.com/Hishamkhashman1/bug-squancher",
-    category: "Current Projects",
-    subCategory: "CLI Tools"
-  }		  
+  },		  
 ];
