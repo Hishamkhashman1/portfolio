1295e77ec2eddde0275d1258a347de72d54a3899
Hisham <hisham.khashman@gmail.com>
2026-07-25 19:24:57 +0900

    added comments to NLP text normalization logic

--- backend/model/inference.py
+++ backend/model/inference.py
@@ -4,9 +4,9 @@ import re
 
 def respond(messages,examples):
     last_message = messages[-1]
-    normalized_text = last_message.lower()
-    normalized_text = re.sub(r'\s+', ' ', last_message).strip
-    normalized_text = re.sub(r'[^\w\s]', '', last_message)
+    normalized_text = last_message.lower()   # this makes everything lowercase ..duh
+    normalized_text = re.sub(r'\s+', ' ', last_message).strip #this removes extra whitespaces, newlines and tabs
+    normalized_text = re.sub(r'[^\w\s]', '', last_message) # this removes punctuation and special chars (keeps numbs and letters dooog)
     
     for example in examples:
         if example in normalized_text:
