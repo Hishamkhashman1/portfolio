eca83baf48a9494883b8daece356c136717d0cb3
Hisham <hisham.khashman@gmail.com>
2026-07-25 22:51:06 +0900

    added NLP , strip and confidence initial design to inherence

--- backend/model/inference.py
+++ backend/model/inference.py
@@ -1,19 +1,43 @@
-from app.schemas import ChatMessage
+from app.schemas import ChatMessage, ChatRequest
 import re
-# add import for examples
+# add import for seeded data of questions and their answers
 
-def respond(messages,examples):
-    last_message = messages[-1]
-    normalized_text = last_message.lower()   # this makes everything lowercase ..duh
-    normalized_text = re.sub(r'\s+', ' ', last_message).strip #this removes extra whitespaces, newlines and tabs
-    normalized_text = re.sub(r'[^\w\s]', '', last_message) # this removes punctuation and special chars (keeps numbs and letters dooog)
+confidence = 0.7
+
+def user_message(messages):
+    user_messages = []
+    for message in messages:
+        if message.role == "user":
+            user_messages.append(message)
+
+    last_message = user_messages[-1]
+
+    return last_message
+
+
+def nlp_text(last_message):
+    lc_text = last_message.lower()   # this makes everything lowercase ..duh
+    rwsnlt_text = re.sub(r'\s+', ' ', lc_text).strip() #this removes extra whitespaces, newlines and tabs
+    normalized_text = re.sub(r'[^\w\s]', '', rwsnlt_text) # this removes punctuation and special chars (keeps numbs and letters dooog)
+
+    return normalized_text
+
+
+words = nlp_text(last_message).split()
+
+def matching_percent(words,seed_data):
+    count = 0
+    for w in words:
+        if w in seed_data:
+            count +=1
+    if count > 0:
+        match_level = count // len(seed_data)
     
-    for example in examples:
-        if example in normalized_text:
-            return example
-        else:
-            return "sorry, I am still learning"
+    return match_level
 
-
-
+def response(match_level,response):
+    if match_level >= confidence:
+        return response
+    else:
+        return "I am not sure, sorry! I am still learning"
 
