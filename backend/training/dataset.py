faef19c633b8bc2ebc12712d5dc8fb170a9eda46
Hisham <hisham.khashman@gmail.com>
2026-07-26 18:00:58 +0900

    feat: added dataset validation

--- backend/training/dataset.py
+++ backend/training/dataset.py
@@ -10,5 +10,15 @@ path_json = Path(__file__).resolve().parents[1] / "data" / "conversations.json"
 with open(path_json, 'r', encoding='utf-8') as file:
     conversations_data = json.load(file)
 
-print(conversations_data)
+#print(conversations_data)
 
+cleaned_data = []
+
+def cleaning_data(conversations_data):
+
+    for items in conversations_data:
+        if 'input_text' in items and 'target_text' in items and items != None:
+            cleaned_data.append(items)
+
+    return cleaned_data
+# print (cleaning_data(conversations_data))
