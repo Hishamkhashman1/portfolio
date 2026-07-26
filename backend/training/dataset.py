b7ef1326c3f6863fb2e6aa876407156f1f215468
Hisham <hisham.khashman@gmail.com>
2026-07-26 17:39:56 +0900

    training dataset minimum

---
+++ backend/training/dataset.py
@@ -0,0 +1,13 @@
+import json
+
+from pathlib import Path
+
+
+path_json = "backend/data/conversations.json"
+
+
+with open(path_json, 'r', encoding='utf-8') as file:
+    conversations_data = json.load(file)
+
+print(conversations_data)
+
